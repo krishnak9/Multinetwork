7ccba6b18fca6d410fcb86a958d9e2bd669f6d34
Krishna <krishnaklooper@gmail.com>
2016-04-02 18:43:14 +0530

    added datetimepicker,simpleform

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'haml', '~> 4.0', '>= 4.0.7'
 gem 'simple_form', '~> 3.2', '>= 3.2.1'
 gem 'devise', '~> 3.5', '>= 3.5.6'
+gem 'acts_as_votable', '~> 0.10.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,44 +1,38 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
-  # GET /posts.json
+
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
+
   def show
   end
 
-  # GET /posts/new
+
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
+
   def edit
   end
 
-  # POST /posts
-  # POST /posts.json
+
   def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+  @post = Post.new(post_params) do |post|
+    post.user = current_user
   end
+  if @post.save
+    redirect_to root_path
+  else
+    redirect_to root_path, notice: @post.errors.full_messages.first
+  end
+end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
+
+
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -51,8 +45,7 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
+
   def destroy
     @post.destroy
     respond_to do |format|
@@ -62,12 +55,12 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def post_params
       params.require(:post).permit(:attachment, :content, :user_id)
     end
